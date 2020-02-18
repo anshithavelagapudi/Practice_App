9f604dfc28e8e6cc69168b7a3a251949fe1d9638
Anshitha <anshithavelagapudi@gmail.com>
2020-02-17 20:05:05 -0600

    commiting changes in code and index files

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 function code()
 {
-     const Total=parseFloat(document.querySelector('#field-total').value)
-     const Rate=parseFloat(document.querySelector('#field-rate').value)
+     const Total=parseFloat(document.querySelector('#field').value)
+     const Rate=parseFloat(document.querySelector('#rate').value)
      console.log(Rate);
      const Tip=Total*(Rate/100)
      const TaxRate=5.5
      const Tax=Total*(TaxRate/100)
      const GrandTotal=Total+Tip+Tax;
-     document.querySelector('#Tip').innerHTML=`$${Tip.toFixed(2)}`
+     document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
      document.querySelector('#Tax').innerHTML=`$${Tax.toFixed(2)}`
      document.querySelector('#Total').innerHTML=`$${GrandTotal.toFixed(2)}`
 }
